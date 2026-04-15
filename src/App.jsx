0f716189f5ee7fef5f9dfeafeccc31045dd5788f
krish koli <krishkoli536@gmail.com>
2026-04-15 15:37:06 +0530

    orbit controls added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import { Canvas } from '@react-three/fiber'
 import React from 'react'
+import "./style.css"
+import { OrbitControls } from '@react-three/drei'
+import * as THREE from 'three'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Canvas>
+        <OrbitControls />
+        <ambientLight />
+        <mesh>
+          <cylinderGeometry args={[1, 1, 1, 30, 30, true]}/>
+          <meshStandardMaterial side={THREE.DoubleSide}/>
+        </mesh>
+      </Canvas>
+    </>
   )
 }
 
-export default App
+export default App;
